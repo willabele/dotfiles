7ac97839dbd38289f1c3cc03b052fd40e6d09c7f
Will Abele <will@willabele.com>
2015-05-17 20:34:39 -0400

    Changes to build vim fedora.

--- build_vim_fedora.sh
+++ build_vim_fedora.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-./configure --prefix=$(HOME) --with-features=huge --enable-gui=gnome2 --enable-multibyte --enable-luainterp
 yum-builddep vim-X11
 git clone https://github.com/nschloe/vim.git
 cd vim
@@ -9,4 +8,3 @@ git checkout tags/v7-4-508
 ./configure --prefix=$HOME --with-features=huge --enable-gui=gnome2 --enable-multibyte --enable-luainterp --enable-pythoninterp --enable-rubyinterp
 make
 make install
-pip install --user powerline-status
